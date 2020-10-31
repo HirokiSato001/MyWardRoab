a8674559d2f9619585e98e479f99b1017c6d839d
HirokiSato001 <hiroki.sato@fujitsu.com>
2020-10-31 17:02:53 +0900

    MonacaCloudを利用してユーザを登録できるように修正

--- www/js/user.js
+++ www/js/user.js
@@ -1,15 +1,36 @@
 // This is a JavaScript file
-
 class User{
   // コンストラクタ
-  constructor(nickName,age){
+  constructor(mailAddress,nickName,age){
+    this.mailAddress = mailAddress;
     this.nickName = nickName;
     this.age = age;
+    this.password = "aaaa";
     this.gender;
   }
+  test(){
+    var MC = monaca.cloud;
+    var email = this.mailAddress;
+    var password = this.password;
+    var age = this.age;
+    var nickName = this.nickName;
+    console.log(MC);
+    
+    MC.User.register(email, password,{age:age,nickName:nickName}).done(function(){
+        console.log('Registration is success!' + MC.User._oid);        
+        document.getElementById('nav').pushPage('menu.html', {});
+      })
+      .fail(function(err)
+      {
+        console.log('FAILED');
+        alert('Registration failed!');
+        console.error(JSON.stringify(err));
+      });
+    }
+
 
   getNickName(){
-    return this.age;
+    return this.nickName;
   }
 
   setGender(gender){
