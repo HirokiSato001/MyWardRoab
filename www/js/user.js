025d680149d2ee5349090629ae8f37f41d755df3
HirokiSato001 <hiroki.sato@fujitsu.com>
2021-03-15 20:42:14 +0900

    ニフクラmobile backendのキー設定を共通化

--- www/js/user.js
+++ www/js/user.js
@@ -10,13 +10,6 @@ var day; // 日
 var gender; // 性別
 var height; // 身長
 
-// ニフクラmobile backendのキー設定
-// API key.
-var applicationKey    = "a5b0d94a415cb1c985e0766684fb926608305759441c8777962112ea428df5ae";
-var clientKey = "c0d33b2d67b4aeb9e608b6c2774965128e10e2c267e06c19654032e4a8f1de2e";
-// SDK initialization.
-var ncmb = new NCMB(applicationKey, clientKey);  
-// var currentUser = ncmb.User.getCurrentUser();
 var currentUser
 class User {
   // コンストラクタ
