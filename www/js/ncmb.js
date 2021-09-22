cf07a608750c6db21844b9c57a7108e37b5e8648
shida.fumiya@fujitsu.com <shida.fumiya@fujitsu.com>
2021-09-22 21:20:51 +0900

    シューズの画像をカテゴリ４に名前変更。ワンピースの画像を削除

--- www/js/ncmb.js
+++ www/js/ncmb.js
@@ -1,8 +1,8 @@
 // This is a JavaScript file
 // ニフクラmobile backendのキー設定
 // API key.
-var applicationKey    = "a5b0d94a415cb1c985e0766684fb926608305759441c8777962112ea428df5ae";
-var clientKey = "c0d33b2d67b4aeb9e608b6c2774965128e10e2c267e06c19654032e4a8f1de2e";
+var applicationKey    = "b1e8e48fd679bd67f24818b60921d655d52b18e8dec293aa115204cec26c0e04";
+var clientKey = "641b33afa7222db62d192a4ce516678eea5acc7043b6d56bdc93c0f7b0cf18fc";
 
 
 // SDK initialization.
